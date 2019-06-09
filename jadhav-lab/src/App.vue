c6379f84351933db085d6a0d2d5663a1338de022
Zhaonan <32943976+LIZHAONAN@users.noreply.github.com>
2019-06-09 01:43:59 -0400

    added dynamic background image

--- jadhav-lab/src/App.vue
+++ jadhav-lab/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app" class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Jadhav Lab</a>
+  <div id="app" class="body">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light rounded border" style="margin: 1em">
+      <a class="navbar-brand">Jadhav Lab</a>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
@@ -30,24 +30,29 @@
       </div>
     </nav>
 
-    <div class="card">
-      <img src="src/images/labgroup.jpg" class="card-img-top">
-    </div>
+    <div class="container">
+      <div class="card">
+        <img src="src/images/labgroup.jpg" class="card-img-top">
+      </div>
 
-    <div class="card">
-      <div class="card-body">
-        <h2 class="card-title"> Learning, Memory and Decision Making in the Mammalian Brain </h2>
-        <p class="card-text">
-          The Jadhav Lab integrates behavior, electrophysiology, optogenetics and computational analysis to investigate the neural basis of learning, memory and decision making in the mammalian brain.
-        </p>
-        <p class="card-text">
-          The brain has a remarkable capacity to learn and to use past experience to guide our daily behavior. 
-          Multiple brain regions coordinate activity to form representations of the external world, learn new experiences, store and retrieve memories, and make decisions. 
-          We are interested in understanding the neural basis of these cognitive abilities by studying processing at the cellular and network level in neuronal circuits of the rodent brain. 
-        </p>
-        <p class="card-text">
-          The Jadhav Lab integrates behavior, electrophysiology, optogenetics and computational analysis to investigate the neural basis of learning, memory and decision making in the mammalian brain.
-        </p>
+      <div class="card">
+        <div class="card-body">
+          <h2 class="card-title"> Learning, Memory and Decision Making in the Mammalian Brain </h2>
+          <p class="card-text">
+            The Jadhav Lab integrates behavior, electrophysiology, optogenetics and computational analysis to investigate the neural basis of learning, memory and decision making in the mammalian brain.
+          </p>
+          <p class="card-text">
+            The brain has a remarkable capacity to learn and to use past experience to guide our daily behavior. 
+            Multiple brain regions coordinate activity to form representations of the external world, learn new experiences, store and retrieve memories, and make decisions. 
+            We are interested in understanding the neural basis of these cognitive abilities by studying processing at the cellular and network level in neuronal circuits of the rodent brain. 
+          </p>
+          <p class="card-text">
+            The Jadhav Lab integrates behavior, electrophysiology, optogenetics and computational analysis to investigate the neural basis of learning, memory and decision making in the mammalian brain.
+          </p>
+        </div>
+      </div>
+    
+      <div class="card">
       </div>
     </div>
   </div>
@@ -71,4 +76,13 @@ export default {
 
 <style>
   @import url('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
+  body{
+    background-image: url("/src/images/funky-lines.png");
+    -webkit-animation: slide 20s linear infinite;
+  }
+
+  @-webkit-keyframes slide {
+    from { background-position: 0 400px; }
+    to { background-position: 400px 0; }
+}
 </style>
