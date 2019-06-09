8365b455d6405ae23118e3a64789ff10bcd05987
Zhaonan <32943976+LIZHAONAN@users.noreply.github.com>
2019-06-08 23:15:54 -0400

    added images, built a starter page

--- jadhav-lab/src/App.vue
+++ jadhav-lab/src/App.vue
@@ -1,60 +1,74 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="app" class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Jadhav Lab</a>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Research</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Publication</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Positions</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">People</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">News</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="card">
+      <img src="src/images/labgroup.jpg" class="card-img-top">
+    </div>
+
+    <div class="card">
+      <div class="card-body">
+        <h2 class="card-title"> Learning, Memory and Decision Making in the Mammalian Brain </h2>
+        <p class="card-text">
+          The Jadhav Lab integrates behavior, electrophysiology, optogenetics and computational analysis to investigate the neural basis of learning, memory and decision making in the mammalian brain.
+        </p>
+        <p class="card-text">
+          The brain has a remarkable capacity to learn and to use past experience to guide our daily behavior. 
+          Multiple brain regions coordinate activity to form representations of the external world, learn new experiences, store and retrieve memories, and make decisions. 
+          We are interested in understanding the neural basis of these cognitive abilities by studying processing at the cellular and network level in neuronal circuits of the rodent brain. 
+        </p>
+        <p class="card-text">
+          The Jadhav Lab integrates behavior, electrophysiology, optogenetics and computational analysis to investigate the neural basis of learning, memory and decision making in the mammalian brain.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import jQuery from 'jquery'
+global.jQuery = require('jquery');
+var $ = global.jQuery;
+window.$ = $;
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Jadhav Lab!'
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  @import url('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
 </style>
